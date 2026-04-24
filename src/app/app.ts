53ba0581faffac2f78cec28c1662768f5cf1c70b
Sergio <shark2150388@gmail.com>
2026-04-24 12:40:33 +0200

    Fase 4: Creado Navbar en Home, añadida pestaña de Favoritos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrls: ['./app.scss'],
 })
-export class App {
-  title = 'movie-finder';
-}
+export class App {}
--- src/app/features/favorites/favorites.component/favorites.component.ts
+++ src/app/features/favorites/favorites.component/favorites.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FavoritesService } from '../../../core/services/favorites.service';
+import { Movie } from '../../../core/models/movie.model';
+import { MovieCardComponent } from '../../../shared/components/movie-card/movie-card.component';
 
 @Component({
-  selector: 'app-favorites.component',
-  imports: [],
+  selector: 'app-favorites',
+  standalone: true,
+  imports: [CommonModule, RouterLink, MovieCardComponent],
   templateUrl: './favorites.component.html',
-  styleUrl: './favorites.component.scss',
+  styleUrls: ['./favorites.component.scss'],
 })
-export class FavoritesComponent {}
+export class FavoritesComponent implements OnInit {
+  favorites: Movie[] = [];
+
+  constructor(
+    private favoritesService: FavoritesService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.loadFavorites();
+  }
+
+  loadFavorites(): void {
+    this.favorites = this.favoritesService.getFavorites();
+    this.cdr.detectChanges();
+  }
+
+  toggleFavorite(movie: Movie): void {
+    this.favorites = this.favoritesService.toggleFavorite(movie);
+    this.cdr.detectChanges();
+  }
+
+  isFavorite(movieId: number): boolean {
+    return this.favorites.some((movie) => movie.id === movieId);
+  }
+}
